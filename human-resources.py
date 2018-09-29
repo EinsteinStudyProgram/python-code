96df9bfc1dad9f453a923e198d02fb3e1373b39e
Administrator <happy_gezz@126.com>
2018-09-30 01:00:23 +0800

    change in home 2018-04-03

--- human-resources.py
+++ human-resources.py
@@ -4,30 +4,34 @@ import time
 
 url='http://apitest.wintalent.cn:8080/wt/xwebservices/externalResumeService?wsdl'
 read_candidate = Client(url)
-pagenumber = 1
-conditionXml='''
-<?xml version="1.0" encoding="UTF-8"?>
-<Condition>
-	<corpCode><![CDATA[hntvhr]]></corpCode>
-	<userName><![CDATA[hntvhr]]></userName>
-	<password><![CDATA[aSfp8pL8U6MnKQrx]]></password>
-<resumeType><![CDATA[2]]></resumeType>
-	<currentPage><![CDATA[3]]></currentPage> 
-	<rowSize><![CDATA[10]]></rowSize>
-	<cType><![CDATA[1]]></cType>
-</Condition>'''
+
 
 try:
-    for pagenumber in range(1,10):
+    for i in range(1,11):
+        pagenumber=i
+        conditionXml = '''
+        <?xml version="1.0" encoding="UTF-8"?>
+        <Condition>
+            <corpCode><![CDATA[hntvhr]]></corpCode>
+            <userName><![CDATA[hntvhr]]></userName>
+            <password><![CDATA[aSfp8pL8U6MnKQrx]]></password>
+            <resumeType><![CDATA[2]]></resumeType>
+            <currentPage><![CDATA[%s]]></currentPage> 
+            <rowSize><![CDATA[10]]></rowSize>
+            <cType><![CDATA[1]]></cType>
+        </Condition>'''%(pagenumber)
         get_condidate_xml = read_candidate.service.readEntryInformation(conditionXml)
-        doc_save = xml.dom.minidom.parseString(get_condidate_xml)
+        print(get_condidate_xml)
+        doc_to_xml = xml.dom.minidom.parseString(get_condidate_xml)
+        print(conditionXml)
         filename = "candidate-%s.xml" % (pagenumber)
         fp = open(filename, 'w')
-        doc_save.writexml(fp, indent='\t', addindent='\t', newl='\n', encoding="gb2312")
+        doc_save = doc_to_xml.toprettyxml(indent="\t", newl="\n", encoding="gbk")
+        doc_save =bytes.decode(doc_save,encoding="gbk")
+        fp.write(doc_save)
+        # doc_save.writexml(fp, indent='\t', addindent='\t', newl='\n', encoding="Unicode")
         fp.close()
 except IOError as e:
     print(e.strerror)
 
-
-print(doc_save)
 
